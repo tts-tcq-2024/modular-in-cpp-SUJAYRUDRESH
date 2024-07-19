55ff3ff4a4c65b3524592a6556f61dd8ddc289fe
SUJAYRUDRESH <60745093+SUJAYRUDRESH@users.noreply.github.com>
2024-07-19 08:35:10 +0530

    Create TelecomWireColorPair.hpp

---
+++ TelecomWireColorPair.hpp
@@ -0,0 +1,12 @@
+#ifndef TELECOM_WIRE_COLOR_PAIR_HPP
+#define TELECOM_WIRE_COLOR_PAIR_HPP
+
+#include <string>
+
+namespace TelecomColorCoding 
+{
+    enum class MajorWireColor { WHITE, RED, BLACK, YELLOW, VIOLET };
+    enum class MinorWireColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
+}
+
+#endif // TELECOM_WIRE_COLOR_PAIR_HPP
