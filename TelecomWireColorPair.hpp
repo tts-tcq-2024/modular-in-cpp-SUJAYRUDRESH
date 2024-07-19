ca4510a6706cbb553df4a5a252993bbe29dbd997
SUJAYRUDRESH <60745093+SUJAYRUDRESH@users.noreply.github.com>
2024-07-19 08:36:29 +0530

    Update TelecomWireColorPair.hpp

--- TelecomWireColorPair.hpp
+++ TelecomWireColorPair.hpp
@@ -7,6 +7,21 @@ namespace TelecomColorCoding
 {
     enum class MajorWireColor { WHITE, RED, BLACK, YELLOW, VIOLET };
     enum class MinorWireColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
+
+    class WireColorPair {
+        private:
+            MajorWireColor majorWireColor;
+            MinorWireColor minorWireColor;
+        public:
+            WireColorPair(MajorWireColor major, MinorWireColor minor);
+            MajorWireColor getMajorWireColor() const;
+            MinorWireColor getMinorWireColor() const;
+            std::string toString() const;
+    };
+
+    WireColorPair getWireColorPairFromNumber(int wirePairNumber);
+    int getNumberFromWireColorPair(MajorWireColor major, MinorWireColor minor);
+    std::string formatColorCodingReferenceManual();
 }
 
 #endif // TELECOM_WIRE_COLOR_PAIR_HPP
